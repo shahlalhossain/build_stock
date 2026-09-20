5036d954ef4ffeec4bf6b2e49b64c2aa65983136
Shahlal <shahlalhossain@gmail.com>
2026-09-20 23:41:21 +0600

    Working on Supplier CRUD Process Update

--- routes/web.php
+++ routes/web.php
@@ -325,4 +325,5 @@ Route::middleware('auth:web')->group(function () {
     Route::get('/audit-log/{type?}', [AuditLogController::class, 'auditLogs'])->name('audit-log')->where('type', 'all|created|updated|deleted');
     Route::delete('/audit-log/{id}/delete', [AuditLogController::class, 'deleteAuditLog'])->name('delete-audit-log');
 
+
 });
